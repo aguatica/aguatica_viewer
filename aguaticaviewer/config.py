b4e2878adb19e734664132f31fdbae537b971d16
chrissikrause <krause.chrs@gmx.de>
2024-10-22 16:51:47 -0600

    Removed sensitive information

--- aguaticaviewer/config.py
+++ aguaticaviewer/config.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+import os
 # aguaticaviewer/config.py
 
 #For EpiCollect API
@@ -6,8 +8,10 @@ CLIENT_SECRET = 's2IQsFWgw9Eygz85DsjBb9RduYOtH1xbWq9smTBq'
 NAME = 'aguatica_viewer'
 SLUG = 'monitoreo-de-agua'
 
+
 #For Google Drive API
-SERVICE_ACCOUNT_FILE = "/Users/christinakrause/Desktop/AguaTica/aguatica-webviewer-f1cd58ba90a1.json"
+load_dotenv()
+SERVICE_ACCOUNT_FILE = os.getenv('GOOGLE_SERVICE_ACCOUNT_FILE')
 SCOPES = ['https://www.googleapis.com/auth/drive']  # Read-only access to Google Drive
 FOLDER_ID = '1t-u10CLuh7LCYjCKFP4W-3LKSzrwN0W4'
 
