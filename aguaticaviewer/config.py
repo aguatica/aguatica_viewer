2fa9448e3babdfb21fed06acaea8309055d733b1
chrissikrause <krause.chrs@gmx.de>
2024-10-22 16:36:17 -0600

    Deleted Google.py

--- aguaticaviewer/config.py
+++ aguaticaviewer/config.py
@@ -7,7 +7,7 @@ NAME = 'aguatica_viewer'
 SLUG = 'monitoreo-de-agua'
 
 #For Google Drive API
-SERVICE_ACCOUNT_FILE = "aguatica-webviewer-f1cd58ba90a1.json"
+SERVICE_ACCOUNT_FILE = "/Users/christinakrause/Desktop/AguaTica/aguatica-webviewer-f1cd58ba90a1.json"
 SCOPES = ['https://www.googleapis.com/auth/drive']  # Read-only access to Google Drive
 FOLDER_ID = '1t-u10CLuh7LCYjCKFP4W-3LKSzrwN0W4'
 
